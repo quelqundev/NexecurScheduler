3b4d09305a352e833452103f82e2d696dc3514d4
quelqun007 <quelqun007@gmail.com>
2018-11-18 17:01:39 +0100

    Add a retry mecanism and a better sms error gestion #22

--- Models/SMSAlert.ts
+++ Models/SMSAlert.ts
@@ -1,6 +1,15 @@
 import { SMSConfig, FreeAccount } from "./SMSConfig";
 import { Log } from "./LogUtil";
 
+export enum SMSAlertStatus {
+    Success,
+    ParametersError,
+    BlockedSpam,
+    AccessDenied,
+    InternalError,
+    UnknownError
+}
+
 export abstract class SMSAlert {
 
     static accounts: Array<FreeAccount> = null;
@@ -10,7 +19,7 @@ export abstract class SMSAlert {
      * 
      * Initialization of Free sms account in order to use API
      */
-    static init (){
+    static init() {
         let config: SMSConfig = require("./../smsconfig.json");
         SMSAlert.accounts = config.freeaccounts;
     }
@@ -19,28 +28,49 @@ export abstract class SMSAlert {
      * Send SMS message to all account specified in smsconfiguration
      * @param msg
      */
-    static sendSMSAlert(msg: string) {
-        if (SMSAlert.accounts == null) SMSAlert.init(); //check SMSAlert has been initialized
-        var sms = require('free-mobile-sms-api');
+    static sendSMSAlert(msg: string): Promise<SMSAlertStatus> {
+        return new Promise((resolve, reject) => {
+            if (SMSAlert.accounts == null) SMSAlert.init(); //check SMSAlert has been initialized
+            var sms = require('free-mobile-sms-api');
 
-        //configuration
-        sms.on('sms:error', function (e) {
-            Log.info(e.code + ": " + e.msg);
-        });
-
-        sms.on('sms:success', function (data) {
-            Log.info("SMS sent !");
-        });
-
-        //for each free account
-        if (SMSAlert.accounts.length > 0) {
-            SMSAlert.accounts.forEach((account: FreeAccount) => {
-                sms.account(account.id, account.key);
-                sms.send(encodeURI(msg));
+            //configuration
+            sms.on('sms:error', function (e) {
+                Log.info(e.code + ": " + e.msg);
+                switch (e.code) {
+                    case 400:
+                        reject(SMSAlertStatus.ParametersError);
+                        break;
+                    case 402:
+                        reject(SMSAlertStatus.BlockedSpam);
+                        break;
+                    case 403:
+                        reject(SMSAlertStatus.AccessDenied);
+                        break;
+                    case 500:
+                        reject(SMSAlertStatus.InternalError);
+                        break;
+                    default:
+                        reject(SMSAlertStatus.UnknownError)
+                        break;
+                }
             });
-        } else {
-            Log.info("You didnt configured your free account into the smsconfig.json file. We can't send SMS.")
-        }
 
+            sms.on('sms:success', function (data) {
+                Log.info("SMS sent !");
+                resolve(SMSAlertStatus.Success);
+            });
+
+            //for each free account
+            if (SMSAlert.accounts.length > 0) {
+                SMSAlert.accounts.forEach((account: FreeAccount) => {
+                    sms.account(account.id, account.key);
+                    sms.send(encodeURI(msg));
+                });
+            } else {
+                Log.info("You didnt configured your free account into the smsconfig.json file. We can't send SMS.");
+                resolve(SMSAlertStatus.Success);
+            }
+
+        })
     }
 }
--- Models/TaskScheduler.ts
+++ Models/TaskScheduler.ts
@@ -1,7 +1,7 @@
 import { Configuration } from "./Configuration";
 import { Log } from "./LogUtil";
 import { AlarmStatus } from "../Nexecur-Unofficial-API/Models/AlarmStatus";
-import { SMSAlert } from "./SMSAlert";
+import { SMSAlert, SMSAlertStatus } from "./SMSAlert";
 import { NexecurAPI } from "../Nexecur-Unofficial-API/Controllers/NexecurAPI";
 import { NoplanningDate } from "./NoplanningDate";
 import { UndefinedAPIError } from "../Nexecur-Unofficial-API/Models/Errors/UndefinedAPIError";
@@ -32,146 +32,162 @@ export class TaskScheduler {
         /**
          * Alarm activation task
          */
-        this.activationSchedule = schedule.scheduleJob({ hour: scheduleConfig.activationHour }, function () {
-            Log.info("--- Activation schedule start ---");
-
-            let postcheckCallback = (status: AlarmStatus) => {
-                switch (status) {
-                    case AlarmStatus.Enabled:
-                        Log.debug('  Alarm state : ENABLED');
-                        Log.info('  Activation reussie !');
-                        break;
-                    case AlarmStatus.Disabled:
-                        //error : alarm is still not enabled
-                        Log.debug('  Alarm state : DISABLED');
-                        Log.info('  Activation echouee !');
-                        SMSAlert.sendSMSAlert(`ACTIVATION ECHOUEE a ${scheduleConfig.activationHour}h. Alarme encore DESACTIVEE.`);
-                        break;
-                    default:
-                        //error : status is not an AlarmStatus
-                        break;
-                }
-                Log.info("--- Activation schedule end ---");
-            };
-
-            let enableCallback = () => {
-                //Check system status after the try
-                Log.debug('Checking alarm status...');
-                NexecurAPI.getAlarmStatus().then((res: AlarmStatus) => {
-                    postcheckCallback(res);
-                }).catch((err: UndefinedAPIError) => {
-                    Log.info(err.name + ' - ' + err.message)
-                });
-            };
-
-            let precheckCallback = ((status: AlarmStatus) => {
-                switch (status) {
-                    case AlarmStatus.Disabled:
-                        Log.debug(' Alarm state : DISABLED');
-                        //Try to enable alarm
-                        Log.debug('  Trying to enable alarm...');
-                        NexecurAPI.enableAlarm().then(() => {
-                            enableCallback();
-                        }).catch((err: OrderAlarmError) => {
-                            Log.info(err.name + ' - ' + err.message)
-                        });
-                        break;
-                    case AlarmStatus.Enabled:
-                        Log.debug(' Alarm state : ENABLED');
-                        //Specific case : alarm already enabled but consider it as normal
-                        break;
-                    default:
-                        //error : status is not an AlarmStatus
-                        break;
-                }
-            });
-
-            //determine if we should activate alarm today or not
-            NoplanningDate.isTodayANoplanningDate((isANoplanningDate: boolean) => {
-                Log.debug("  isANoplanningDate : " + isANoplanningDate);
-                if (isANoplanningDate == true) {
-                    //we should not activate the security system...
-                    Log.info("  Today is an exception, dont activate alarm");
-                    //...and then dont desactivate it next time
-                    this.desactivationSchedule.cancelNext();
-                } else {
-                    //Check system status before trying to enable
-                    Log.debug("  checking alarm status...");
-                    NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
-                        precheckCallback(result);
-                    }).catch((err: UndefinedAPIError) => {
-                        Log.info(err.name + ' - ' + err.message)
-                    });
-                }
-            });
-
-
-        });
+        this.activationSchedule = schedule.scheduleJob({ hour: scheduleConfig.activationHour }, this.activationFunction);
 
         /**
          * Alarm desactivation task
          */
-        this.desactivationSchedule = schedule.scheduleJob({ hour: scheduleConfig.desactivationHour }, function () {
-            Log.info("--- Desactivation schedule start ---");
+        this.desactivationSchedule = schedule.scheduleJob({ hour: scheduleConfig.desactivationHour }, this.desactivationFunction);
+    }
 
-            let postcheckCallback = (status: AlarmStatus) => {
-                switch (status) {
-                    case AlarmStatus.Disabled:
-                        Log.debug(' Alarm state : DISABLED');
-                        Log.info(' Desactivation reussie !');
-                        break;
-                    case AlarmStatus.Enabled:
-                        Log.debug(' Alarm state : ENABLED');
-                        Log.info(' Desactivation echouee...');
-                        //error : alarm is still enabled
-                        SMSAlert.sendSMSAlert(`URGENT ! DESACTIVATION ECHOUEE a ${scheduleConfig.desactivationHour}h. Alarme encore ACTIVEE. Veuillez utiliser l'application Mon Nexecur pour desarmer.`);
-                        break;
-                    default:
-                        //error : status is not an AlarmStatus
-                        break;
-                }
-                Log.info("--- Desactivation schedule end ---");
-            };
+    /**
+     * Enable alarm security system
+     */
+    activationFunction() {
+        Log.info("--- Activation schedule start ---");
 
-            let disableCallback = () => {
-                //Check system status after the try
-                Log.debug(' Checking alarm status');
-                NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
-                    postcheckCallback(result);
-                }).catch((err: UndefinedAPIError) => {
-                    Log.info(err.name + ' - ' + err.message)
-                });
-            };
+        let postcheckCallback = (status: AlarmStatus) => {
+            switch (status) {
+                case AlarmStatus.Enabled:
+                    Log.debug('  Alarm state : ENABLED');
+                    Log.info('  Activation reussie !');
+                    break;
+                case AlarmStatus.Disabled:
+                    //error : alarm is still not enabled
+                    Log.debug('  Alarm state : DISABLED');
+                    Log.info('  Activation echouee !');
+                    SMSAlert.sendSMSAlert(`ACTIVATION ECHOUEE a ${this.scheduleConfig.activationHour}h. Alarme encore DESACTIVEE.`);
+                    break;
+                default:
+                    //error : status is not an AlarmStatus
+                    break;
+            }
+            Log.info("--- Activation schedule end ---");
+        };
 
-            let precheckCallback = ((status: AlarmStatus) => {
-                switch (status) {
-                    case AlarmStatus.Enabled:
-                        Log.debug(' Alarm state : ENABLED');
-                        //Try to disable alarm
-                        Log.debug(' Trying to desactivate alarm...');
-                        NexecurAPI.disableAlarm().then(() => {
-                            disableCallback();
-                        }).catch((err: OrderAlarmError) => {
-                            Log.info(err.name + ' - ' + err.message)
-                        });
-                        break;
-                    case AlarmStatus.Disabled:
-                        Log.debug(' Alarm state : DISABLED');
-                        //Specific case : alarm already disabled
-                        break;
-                    default:
-                        //error : status is not an AlarmStatus
-                        break;
-                }
-            });
-
-            //Check system status before trying to enable
-            Log.debug(' Checking alarm status');
-            NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
-                precheckCallback(result);
+        let enableCallback = () => {
+            //Check system status after the try
+            Log.debug('Checking alarm status...');
+            NexecurAPI.getAlarmStatus().then((res: AlarmStatus) => {
+                postcheckCallback(res);
             }).catch((err: UndefinedAPIError) => {
                 Log.info(err.name + ' - ' + err.message)
             });
+        };
+
+        let precheckCallback = ((status: AlarmStatus) => {
+            switch (status) {
+                case AlarmStatus.Disabled:
+                    Log.debug(' Alarm state : DISABLED');
+                    //Try to enable alarm
+                    Log.debug('  Trying to enable alarm...');
+                    NexecurAPI.enableAlarm().then(() => {
+                        enableCallback();
+                    }).catch((err: OrderAlarmError) => {
+                        Log.info(err.name + ' - ' + err.message)
+                    });
+                    break;
+                case AlarmStatus.Enabled:
+                    Log.debug(' Alarm state : ENABLED');
+                    //Specific case : alarm already enabled but consider it as normal
+                    break;
+                default:
+                    //error : status is not an AlarmStatus
+                    break;
+            }
+        });
+
+        //determine if we should activate alarm today or not
+        NoplanningDate.isTodayANoplanningDate((isANoplanningDate: boolean) => {
+            Log.debug("  isANoplanningDate : " + isANoplanningDate);
+            if (isANoplanningDate == true) {
+                //we should not activate the security system...
+                Log.info("  Today is an exception, dont activate alarm");
+                //...and then dont desactivate it next time
+                this.desactivationSchedule.cancelNext();
+            } else {
+                //Check system status before trying to enable
+                Log.debug("  checking alarm status...");
+                NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
+                    precheckCallback(result);
+                }).catch((err: UndefinedAPIError) => {
+                    Log.info(err.name + ' - ' + err.message)
+                });
+            }
+        });
+
+
+    }
+
+    /**
+     * Disable alarm security system
+     */
+    desactivationFunction() {
+        Log.info("--- Desactivation schedule start ---");
+
+        let postcheckCallback = (status: AlarmStatus) => {
+            switch (status) {
+                case AlarmStatus.Disabled:
+                    Log.debug(' Alarm state : DISABLED');
+                    Log.info(' Desactivation reussie !');
+                    break;
+                case AlarmStatus.Enabled:
+                    Log.debug(' Alarm state : ENABLED');
+                    Log.info(' Desactivation echouee...');
+                    //error : alarm is still enabled
+                    SMSAlert
+                        .sendSMSAlert(`URGENT ! DESACTIVATION ECHOUEE a ${this.scheduleConfig.desactivationHour}h. Alarme encore ACTIVEE. Veuillez utiliser l'application Mon Nexecur pour desarmer.`)
+                        .catch((status: SMSAlertStatus) => {
+                            Log.info("Erreur lors de l envoi du SMS : " + status);
+                            //we failed at disabling alarm, let's retry
+                            this.desactivationFunction();
+                        });
+                    break;
+                default:
+                    //error : status is not an AlarmStatus
+                    break;
+            }
+            Log.info("--- Desactivation schedule end ---");
+        };
+
+        let disableCallback = () => {
+            //Check system status after the try
+            Log.debug(' Checking alarm status');
+            NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
+                postcheckCallback(result);
+            }).catch((err: UndefinedAPIError) => {
+                Log.info(err.name + ' - ' + err.message)
+            });
+        };
+
+        let precheckCallback = ((status: AlarmStatus) => {
+            switch (status) {
+                case AlarmStatus.Enabled:
+                    Log.debug(' Alarm state : ENABLED');
+                    //Try to disable alarm
+                    Log.debug(' Trying to desactivate alarm...');
+                    NexecurAPI.disableAlarm().then(() => {
+                        disableCallback();
+                    }).catch((err: OrderAlarmError) => {
+                        Log.info(err.name + ' - ' + err.message)
+                    });
+                    break;
+                case AlarmStatus.Disabled:
+                    Log.debug(' Alarm state : DISABLED');
+                    //Specific case : alarm already disabled
+                    break;
+                default:
+                    //error : status is not an AlarmStatus
+                    break;
+            }
+        });
+
+        //Check system status before trying to enable
+        Log.debug(' Checking alarm status');
+        NexecurAPI.getAlarmStatus().then((result: AlarmStatus) => {
+            precheckCallback(result);
+        }).catch((err: UndefinedAPIError) => {
+            Log.info(err.name + ' - ' + err.message)
         });
     }
 }
